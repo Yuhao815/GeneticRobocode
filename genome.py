5c8ad688a422b55d5d312696264bcd6fdc0f4efb
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2015-11-15 15:15:45 -0600

    moved genome methods into their own file

---
+++ genome.py
@@ -0,0 +1,54 @@
+def TurnGunToEnemy():
+
+def TurnGunLeft5():
+
+def TurnGunLeft10():
+
+def TurnGunRight5():
+
+def TurnGunRight10():
+
+def Fire1():
+
+def Fire2():
+
+def Fire3():
+
+#Tests
+def TestEnemyEnergy():
+
+def TestEnemyEnergy0():
+
+def TestEnemyEnergyBelow10():
+
+def TestEnergyBelow10():
+
+def TestEnergyGreaterThanEnemys():
+
+def TestEnergyLessThanEnemys():
+
+def TestEnemyWithin10Ticks():
+
+def TestEnemyWithin20Ticks():
+
+def TestEnemyWithin50Ticks():
+
+def TestGunIsHot():
+
+def TestGunWithin5Ticks():
+
+def TestTurnToEnemyWithin10Ticks():
+
+def TestTurnToEnemyWithin5tTcks():
+
+
+
+def TurnRight10():
+
+def Or(test1, test2, action): #returns bool
+
+def And(test1, test2, antion):
+
+def Not(test, action):
+
+def If_Then(test, action):
