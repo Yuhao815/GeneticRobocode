1ee087d346d43a0207353ed2bd760c65ef847102
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2015-11-15 18:12:09 -0600

    added pass into empty methods

--- genome.py
+++ genome.py
@@ -1,53 +1,54 @@
 #Action
 def TurnGunToEnemy():
-
+	pass
 def TurnGunLeft5():
-
+	pass
 def TurnGunLeft10():
-
+	pass
 def TurnGunRight5():
-
+	pass
 def TurnGunRight10():
-
+	pass
 def Fire1():
-
+	pass
 def Fire2():
-
+	pass
 def Fire3():
-
+	pass
 #Tests
 def TestEnemyEnergy():
-
+	pass
 def TestEnemyEnergy0():
-
+	pass
 def TestEnemyEnergyBelow10():
-
+	pass
 def TestEnergyBelow10():
-
+	pass
 def TestEnergyGreaterThanEnemys():
-
+	pass
 def TestEnergyLessThanEnemys():
-
+	pass
 def TestEnemyWithin10Ticks():
-
+	pass
 def TestEnemyWithin20Ticks():
-
+	pass
 def TestEnemyWithin50Ticks():
-
+	pass
 def TestGunIsHot():
-
+	pass
 def TestGunWithin5Ticks():
-
+	pass
 def TestTurnToEnemyWithin10Ticks():
-
+	pass
 def TestTurnToEnemyWithin5TIcks():
-
+	pass
 
 # Logic
 def Or(test1, test2, action): #returns bool
-
+	pass
 def And(test1, test2, antion):
-
+	pass
 def Not(test, action):
-
+	pass
 def If_Then(test, action):
+	pass
