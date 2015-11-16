b7c91b007c44655b25373c103c9110839ae37861
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2015-11-15 18:28:46 -0600

    Working to get compilable. (still throws errors)

--- pset.py
+++ pset.py
@@ -1,6 +1,6 @@
 import genome
 
-import numpy
+#import numpy
 
 from deap import algorithms
 from deap import base
@@ -8,38 +8,40 @@ from deap import creator
 from deap import tools
 from deap import gp
 
-pset = gp.PrimitiveSetTyped("main", [], types.NoneType)
+NoneType = type(None)
+
+pset = gp.PrimitiveSetTyped("main", [], NoneType)
 
 #logical
-pset.addPrimitive(Or, [bool, bool, types.NoneType], types.NoneType)
-pset.addPrimitive(And, [bool, bool, types.NoneType], types.NoneType)
-pset.addPrimitive(Not, [bool, types.NoneType], types.NoneType)
-pset.addPrimitive(If_Then, [bool, types.NoneType], types.NoneType)
+pset.addPrimitive(genome.Or, [bool, bool, NoneType], NoneType)
+pset.addPrimitive(genome.And, [bool, bool, NoneType], NoneType)
+pset.addPrimitive(genome.Not, [bool, NoneType], NoneType)
+pset.addPrimitive(genome.If_Then, [bool, NoneType], NoneType)
 
 #actions
-pset.addTerminal(TurnGunToEnemy, types.NoneType)
-pset.addTerminal(TurnLeft5, types.NoneType)
-pset.addTerminal(TurnLeft10, types.NoneType)
-pset.addTerminal(TurnRight5, types.NoneType)
-pset.addTerminal(TurnRight10, types.NoneType)
-pset.addTerminal(Fire1, types.NoneType)
-pset.addTerminal(Fire2, types.NoneType)
-pset.addTerminal(Fire3,types.NoneType)
+pset.addTerminal(genome.TurnGunToEnemy, NoneType)
+pset.addTerminal(genome.TurnGunLeft5, NoneType)
+pset.addTerminal(genome.TurnGunLeft10, NoneType)
+pset.addTerminal(genome.TurnGunRight5, NoneType)
+pset.addTerminal(genome.TurnGunRight10, NoneType)
+pset.addTerminal(genome.Fire1, NoneType)
+pset.addTerminal(genome.Fire2, NoneType)
+pset.addTerminal(genome.Fire3, NoneType)
 
 #tests
-pset.addTerminal(TestEnemyEnergy, bool)
-pset.addTerminal(TestEnemyEnergy0, bool)
-pset.addTerminal(TestEnemyEnergyBelow10, bool)
-pset.addTerminal(TestEnergyBelow10, bool)
-pset.addTerminal(TestEnergyGreaterThanEnemys, bool)
-pset.addTerminal(TestEnergyLessThanEnemys, bool)
-pset.addTerminal(TestEnemyWithin10Ticks, bool)
-pset.addTerminal(TestEnemyWithin20Ticks, bool)
-pset.addTerminal(TestEnemyWithin50Ticks, bool)
-pset.addTerminal(TestGunIsHot, bool)
-pset.addTerminal(TestGunWithin5Ticks, bool)
-pset.addTerminal(TestTurnToEnemyWithin10Ticks, bool)
-pset.addTerminal(TestTurnToEnemyWithin5TIcks, bool)
+pset.addTerminal(genome.TestEnemyEnergy, bool)
+pset.addTerminal(genome.TestEnemyEnergy0, bool)
+pset.addTerminal(genome.TestEnemyEnergyBelow10, bool)
+pset.addTerminal(genome.TestEnergyBelow10, bool)
+pset.addTerminal(genome.TestEnergyGreaterThanEnemys, bool)
+pset.addTerminal(genome.TestEnergyLessThanEnemys, bool)
+pset.addTerminal(genome.TestEnemyWithin10Ticks, bool)
+pset.addTerminal(genome.TestEnemyWithin20Ticks, bool)
+pset.addTerminal(genome.TestEnemyWithin50Ticks, bool)
+pset.addTerminal(genome.TestGunIsHot, bool)
+pset.addTerminal(genome.TestGunWithin5Ticks, bool)
+pset.addTerminal(genome.TestTurnToEnemyWithin10Ticks, bool)
+pset.addTerminal(genome.TestTurnToEnemyWithin5TIcks, bool)
 
 #begin genetic programming
 #initialize population
@@ -54,7 +56,7 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 def evalRobot(individual):
     # Transform the tree expression in a callable function
     #func = toolbox.compile(expr=individual)
-    
+    print("eval!")
     result = 0 #call robocode
     return result
 
@@ -65,16 +67,13 @@ toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
-def main():
-    random.seed(10)
-    pop = toolbox.population(n=100)
-    hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("std", numpy.std)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
-    
-    algorithms.eaSimple(pop, toolbox, 0.5, 0.2, 40, stats, halloffame=hof)
+#random.seed(10)
+pop = toolbox.population(n=100)
+hof = tools.HallOfFame(1)
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+#stats.register("avg", numpy.mean)
+#stats.register("std", numpy.std)
+#stats.register("min", numpy.min)
+#stats.register("max", numpy.max)
 
-    return pop, stats, hof
+algorithms.eaSimple(pop, toolbox, 0.5, 0.2, 40, stats, halloffame=hof)
