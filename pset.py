d7aa12bc1a83848759516036808cb585b1ef990e
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2015-11-16 22:12:07 -0600

    Changed the terminals back to primitives and added true terminals
    Compilable!

--- pset.py
+++ pset.py
@@ -19,29 +19,33 @@ pset.addPrimitive(genome.Not, [bool, NoneType], NoneType)
 pset.addPrimitive(genome.If_Then, [bool, NoneType], NoneType)
 
 #actions
-pset.addTerminal(genome.TurnGunToEnemy, NoneType)
-pset.addTerminal(genome.TurnGunLeft5, NoneType)
-pset.addTerminal(genome.TurnGunLeft10, NoneType)
-pset.addTerminal(genome.TurnGunRight5, NoneType)
-pset.addTerminal(genome.TurnGunRight10, NoneType)
-pset.addTerminal(genome.Fire1, NoneType)
-pset.addTerminal(genome.Fire2, NoneType)
-pset.addTerminal(genome.Fire3, NoneType)
+pset.addPrimitive(genome.TurnGunToEnemy, [], NoneType)
+pset.addPrimitive(genome.TurnGunLeft5, [], NoneType)
+pset.addPrimitive(genome.TurnGunLeft10, [], NoneType)
+pset.addPrimitive(genome.TurnGunRight5, [], NoneType)
+pset.addPrimitive(genome.TurnGunRight10, [], NoneType)
+pset.addPrimitive(genome.Fire1, [], NoneType)
+pset.addPrimitive(genome.Fire2, [], NoneType)
+pset.addPrimitive(genome.Fire3, [], NoneType)
 
 #tests
-pset.addTerminal(genome.TestEnemyEnergy, bool)
-pset.addTerminal(genome.TestEnemyEnergy0, bool)
-pset.addTerminal(genome.TestEnemyEnergyBelow10, bool)
-pset.addTerminal(genome.TestEnergyBelow10, bool)
-pset.addTerminal(genome.TestEnergyGreaterThanEnemys, bool)
-pset.addTerminal(genome.TestEnergyLessThanEnemys, bool)
-pset.addTerminal(genome.TestEnemyWithin10Ticks, bool)
-pset.addTerminal(genome.TestEnemyWithin20Ticks, bool)
-pset.addTerminal(genome.TestEnemyWithin50Ticks, bool)
-pset.addTerminal(genome.TestGunIsHot, bool)
-pset.addTerminal(genome.TestGunWithin5Ticks, bool)
-pset.addTerminal(genome.TestTurnToEnemyWithin10Ticks, bool)
-pset.addTerminal(genome.TestTurnToEnemyWithin5TIcks, bool)
+pset.addPrimitive(genome.TestEnemyEnergy, [], bool)
+pset.addPrimitive(genome.TestEnemyEnergy0, [], bool)
+pset.addPrimitive(genome.TestEnemyEnergyBelow10, [], bool)
+pset.addPrimitive(genome.TestEnergyBelow10, [], bool)
+pset.addPrimitive(genome.TestEnergyGreaterThanEnemys, [], bool)
+pset.addPrimitive(genome.TestEnergyLessThanEnemys, [], bool)
+pset.addPrimitive(genome.TestEnemyWithin10Ticks, [], bool)
+pset.addPrimitive(genome.TestEnemyWithin20Ticks, [], bool)
+pset.addPrimitive(genome.TestEnemyWithin50Ticks, [], bool)
+pset.addPrimitive(genome.TestGunIsHot, [], bool)
+pset.addPrimitive(genome.TestGunWithin5Ticks, [], bool)
+pset.addPrimitive(genome.TestTurnToEnemyWithin10Ticks, [], bool)
+pset.addPrimitive(genome.TestTurnToEnemyWithin5TIcks, [], bool)
+
+pset.addTerminal(True, bool)
+pset.addTerminal(False, bool)
+pset.addTerminal(None, NoneType)
 
 #begin genetic programming
 #initialize population
@@ -57,8 +61,8 @@ def evalRobot(individual):
     # Transform the tree expression in a callable function
     #func = toolbox.compile(expr=individual)
     print("eval!")
-    result = 0 #call robocode
-    return result
+    result = 1.0 #call robocode
+    return result,
 
 #genetic parameters
 toolbox.register("evaluate", evalRobot)
